1b6ccb2ce2bb4055e439b34ed39ad0e82824a07c
toasty . <asaeasaeasae.x10@gmail.com>
2024-04-07 11:22:20 +0000

    fix scripts, add gitpod init

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@ export SCRIPT_AES256_ENCRYPTION_KEY=$(cat godot.gdkey)
 
 oldPWD=$PWD
 godotDir=$PWD/godot
+export ANDROID_HOME=$PWD/cmdline-tools
 cd $godotDir
 
 echo "1" | sudo update-alternatives --config x86_64-w64-mingw32-g++ # patch for compiling windows from ubuntu
@@ -15,18 +16,16 @@ scons platform=linuxbsd target=template_debug arch=x86_64 module_mono_enabled=ye
 scons platform=windows target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 scons platform=windows target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 
-scons platform=android target=template_release arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed
-scons platform=android target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
-cd $godotDir/platform/android/java
+scons platform=android target=template_release arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
+scons platform=android target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
+cd $godotDir/platform/android/java &&\
 ./gradlew generateGodotTemplates
-./gradlew clean
 cd $godotDir
 
-scons platform=android target=template_debug arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed
-scons platform=android target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
-cd $godotDir/platform/android/java
+scons platform=android target=template_debug arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
+scons platform=android target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
+cd $godotDir/platform/android/java &&\
 ./gradlew generateGodotTemplates
-./gradlew clean
 cd $godotDir
 
 strip bin/*
--- setup.sh
+++ setup.sh
@@ -19,20 +19,27 @@ sudo apt-get install \
 #win
 sudo apt-get install mingw-w64
 #android
-wget "https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip" -P /tmp
-unzip "/tmp/commandlinetools-linux-11076708_latest.zip"
-curl -s "https://get.sdkman.io" | bash
+if [ ! -d $PWD/cmdline-tools ]; then
+  wget "https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip" -P /tmp
+  unzip "/tmp/commandlinetools-linux-11076708_latest.zip"
+fi
+curl -s https://get.sdkman.io | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 17.0.10-tem
-sdk install gradle 7.4.2
+sdk install gradle
 
 #setup
 #android
 export ANDROID_HOME=$PWD/cmdline-tools
 source ~/.bashrc
-yes | $ANDROID_HOME/bin/sdkmanager --sdk_root=$ANDROID_HOME --licenses
+$ANDROID_HOME/bin/sdkmanager --sdk_root=$ANDROID_HOME --licenses
 $ANDROID_HOME/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;33.0.2" "platforms;android-33" "cmdline-tools;latest" "cmake;3.10.2.4988404" "ndk;23.2.8568313"
 $ANDROID_HOME/bin/sdkmanager --sdk_root=$ANDROID_HOME "platform-tools" "build-tools;30.0.3" "platforms;android-29" "cmdline-tools;latest" "cmake;3.10.2.4988404"
 
 
 # clone godot source
-git clone --branch 4.2.1-stable "https://github.com/godotengine/godot.git"
+if [ ! -d $PWD/godot ]; then
+  git clone --branch 4.2.1-stable "https://github.com/godotengine/godot.git"
+else
+  echo "Godot directory already exists, not cloning."
+fi
