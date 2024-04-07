53e2b49516c51c4f613e15a39f79787cf91a8f32
toasty . <asaeasaeasae.x10@gmail.com>
2024-04-07 13:13:40 +0000

    add echos

--- compile.sh
+++ compile.sh
@@ -7,27 +7,33 @@ cd $godotDir
 
 echo "1" | sudo update-alternatives --config x86_64-w64-mingw32-g++ # patch for compiling windows from ubuntu
 
+echo \#\#\#\#\# Building editors \#\#\#\#\#
 scons platform=linuxbsd target=editor arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 scons platform=windows target=editor arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 
+echo \#\#\#\#\# Building linux templates \#\#\#\#\#
 scons platform=linuxbsd target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 scons platform=linuxbsd target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 
+echo \#\#\#\#\# Building windows templates \#\#\#\#\#
 scons platform=windows target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 scons platform=windows target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed
 
+echo \#\#\#\#\# Building android release template \#\#\#\#\#
 scons platform=android target=template_release arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
 scons platform=android target=template_release arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
 cd $godotDir/platform/android/java &&\
 ./gradlew generateGodotTemplates
 cd $godotDir
 
+echo \#\#\#\#\# Building android debug template \#\#\#\#\#
 scons platform=android target=template_debug arch=arm64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
 scons platform=android target=template_debug arch=x86_64 module_mono_enabled=yes lto=full production=yes optimize=speed &&\
 cd $godotDir/platform/android/java &&\
 ./gradlew generateGodotTemplates
 cd $godotDir
 
+echo \#\#\#\#\# Stripping binaries \#\#\#\#\#
 strip bin/*
 
 cd $oldPWd
